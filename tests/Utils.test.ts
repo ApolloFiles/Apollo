dbeeb081a2ccdf9d54f8d459403b513a0764097a
Christian Koop <contact@sprax2013.de>
2022-04-24 03:57:17 +0200

    Improve unit tests for `Utils#prettifyFileSize`

--- tests/Utils.test.ts
+++ tests/Utils.test.ts
@@ -1,6 +1,8 @@
 import Utils from '../src/Utils';
 
 describe('#prettifyFileSize', () => {
+  const expectedErrorMessage = 'The given bytes need to be a positive number';
+
   test('For non SI units', () => {
     expect(Utils.prettifyFileSize(0)).toBe('0 B');
     expect(Utils.prettifyFileSize(1)).toBe('1 B');
@@ -13,6 +15,7 @@ describe('#prettifyFileSize', () => {
     expect(Utils.prettifyFileSize(Math.pow(1024, 7))).toBe('1.00 ZiB');
     expect(Utils.prettifyFileSize(Math.pow(1024, 8))).toBe('1.00 YiB');
 
+    expect(Utils.prettifyFileSize(Math.pow(1024, 9))).toBe('1024.00 YiB');
     expect(Utils.prettifyFileSize(Math.pow(1024, 10))).toBe('1048576.00 YiB');
 
     expect(Utils.prettifyFileSize(1024 + 512)).toBe('1.50 KiB');
@@ -30,28 +33,29 @@ describe('#prettifyFileSize', () => {
     expect(Utils.prettifyFileSize(Math.pow(1000, 7), true)).toBe('1.00 ZB');
     expect(Utils.prettifyFileSize(Math.pow(1000, 8), true)).toBe('1.00 YB');
 
+    expect(Utils.prettifyFileSize(Math.pow(1000, 9), true)).toBe('1000.00 YB');
     expect(Utils.prettifyFileSize(Math.pow(1000, 10), true)).toBe('1000000.00 YB');
 
     expect(Utils.prettifyFileSize(1500, true)).toBe('1.50 kB');
   });
 
   test('With negative bytes', () => {
-    expect(() => Utils.prettifyFileSize(-1)).toThrowError();
-    expect(() => Utils.prettifyFileSize(Number.MIN_SAFE_INTEGER)).toThrowError();
+    expect(() => Utils.prettifyFileSize(-1)).toThrowError(expectedErrorMessage);
+    expect(() => Utils.prettifyFileSize(Number.MIN_SAFE_INTEGER)).toThrowError(expectedErrorMessage);
 
-    expect(() => Utils.prettifyFileSize(-1, true)).toThrowError();
-    expect(() => Utils.prettifyFileSize(Number.MIN_SAFE_INTEGER, true)).toThrowError();
+    expect(() => Utils.prettifyFileSize(-1, true)).toThrowError(expectedErrorMessage);
+    expect(() => Utils.prettifyFileSize(Number.MIN_SAFE_INTEGER, true)).toThrowError(expectedErrorMessage);
   });
 
   test('With invalid values for bytes', () => {
-    expect(() => Utils.prettifyFileSize(Number.NaN)).toThrowError();
-    expect(() => Utils.prettifyFileSize(undefined as any)).toThrowError();
-    expect(() => Utils.prettifyFileSize(null as any)).toThrowError();
-    expect(() => Utils.prettifyFileSize('10' as any)).toThrowError();
+    expect(() => Utils.prettifyFileSize(Number.NaN)).toThrowError(expectedErrorMessage);
+    expect(() => Utils.prettifyFileSize(undefined as any)).toThrowError(expectedErrorMessage);
+    expect(() => Utils.prettifyFileSize(null as any)).toThrowError(expectedErrorMessage);
+    expect(() => Utils.prettifyFileSize('10' as any)).toThrowError(expectedErrorMessage);
 
-    expect(() => Utils.prettifyFileSize(Number.NaN, true)).toThrowError();
-    expect(() => Utils.prettifyFileSize(undefined as any, true)).toThrowError();
-    expect(() => Utils.prettifyFileSize(null as any, true)).toThrowError();
-    expect(() => Utils.prettifyFileSize('10' as any, true)).toThrowError();
+    expect(() => Utils.prettifyFileSize(Number.NaN, true)).toThrowError(expectedErrorMessage);
+    expect(() => Utils.prettifyFileSize(undefined as any, true)).toThrowError(expectedErrorMessage);
+    expect(() => Utils.prettifyFileSize(null as any, true)).toThrowError(expectedErrorMessage);
+    expect(() => Utils.prettifyFileSize('10' as any, true)).toThrowError(expectedErrorMessage);
   });
 });
