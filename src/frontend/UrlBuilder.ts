18eac41fed4c59d854321c651b58bb48ec3d3b42
Christian Koop <contact@sprax2013.de>
2022-04-24 02:31:20 +0200

    Adds an basic url builder for frontend urls
    Currently only able to generate urls for files in the default and trash virtual file system

---
+++ src/frontend/UrlBuilder.ts
@@ -0,0 +1,21 @@
+import Fs from 'fs';
+import Path from 'path';
+import IUserFile from '../files/IUserFile';
+
+export default class UrlBuilder {
+  static async buildUrl(file: IUserFile, fileStat?: Fs.Stats): Promise<string> {
+    let basePath;
+
+    if (file.getFileSystem() == file.getOwner().getDefaultFileSystem()) {
+      basePath = `/browse/`;
+    } else if (file.getFileSystem() == file.getOwner().getTrashBinFileSystem()) {
+      basePath = `/trash/`;
+    } else {
+      throw new Error('Cannot generate url for given file system');
+    }
+
+    const isDirectory = fileStat?.isDirectory() ?? await file.isDirectory();
+
+    return Path.join(basePath, encodeURI(file.getPath()), isDirectory ? '/' : '');
+  }
+}
