2c0d50d902dfd0537156cee82aae466d2adcb83f
Christian Koop <contact@sprax2013.de>
2022-04-24 03:55:04 +0200

    Fixes a couple of bugs in `Utils#prettifyFileSize`
    * Negative/Invalid inputs for `bytes`
    * SI units not correctly calculated because `1024` is still used instead of 1000
    * Values than would be bigger than `YB`/`YiB` were converted into a higher unit but the output could be `1.00 undefined`, now it is capped to `YB`/`YiB`

--- src/Utils.ts
+++ src/Utils.ts
@@ -60,16 +60,23 @@ export default class Utils {
   }
 
   static prettifyFileSize(bytes: number, si: boolean = false): string {
+    if (bytes < 0 || !Number.isFinite(bytes)) {
+      throw new Error('The given bytes need to be a positive number');
+    }
+
+    const base = si ? 1000 : 1024;
     const units = si ?
         ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] :
         ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
 
-    let i = Math.floor(Math.log(bytes) / Math.log(1024));
+    let i = Math.floor(Math.log(bytes) / Math.log(base));
     if (i < 0) {
       i = 0;
+    } else if (i >= units.length) {
+      i = units.length - 1;
     }
 
-    return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + units[i];
+    return (bytes / Math.pow(base, i)).toFixed(i > 0 ? 2 : 0) + ' ' + units[i];
   }
 
   static tryReplacingBadCharactersForFileName(fileName: string): string {
