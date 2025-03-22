392bf17c12317f98c1185c5dcede76fced147afa
Christian Koop <contact@sprax2013.de>
2025-03-22 15:46:28 +0100

    fix(frontend): move <main> tag base layout to individual pages
    This gives more control of the resulting HTML down the line.
    Not everything on a page is automatically the 'main' thing.

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -5,11 +5,13 @@
 
     const {children} = $props();
 
+    // TODO: Mal https://smashing-freiburg-2024.netlify.app/07-color-scheme/ anschauen (+ Folie danach)
+    // TODO: Mal https://smashing-freiburg-2024.netlify.app/20-text-wrap/ und https://smashing-freiburg-2024.netlify.app/31-text-box-trim/ anschauen
+    // TODO: Mal https://smashing-freiburg-2024.netlify.app/27-dialog/ und https://smashing-freiburg-2024.netlify.app/28-popover/ anschauen
+    // TODO: Für Video-Player-Styles anschauen: https://smashing-freiburg-2024.netlify.app/29-anchor/
     onMount(() => {
         import('bootstrap/dist/js/bootstrap.bundle.js');
     });
 </script>
 
-<main>
-    {@render children()}
-</main>
+{@render children()}
