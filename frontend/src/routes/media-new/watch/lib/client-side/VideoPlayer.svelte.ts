9bcfec92a458ae9f12903582d995dd8f16442112
Christian Koop <contact@sprax2013.de>
2025-04-04 20:45:53 +0200

    refactor(frontend): move private VideoPlayer#setupEventListeners down
    + removed debug logging of the events

--- frontend/src/routes/media-new/watch/lib/client-side/VideoPlayer.svelte.ts
+++ frontend/src/routes/media-new/watch/lib/client-side/VideoPlayer.svelte.ts
@@ -28,49 +28,6 @@ export default class VideoPlayer {
     this.setupEventListeners();
   }
 
-  private setupEventListeners(): void {
-    this.backend.addPassiveEventListener('loadedmetadata', () => {
-      console.log('event: loadedmetadata');
-      this.duration = this.backend.duration;
-      this.localBufferedRanges = this.backend.getBufferedRanges();
-      this.remoteBufferedRange = this.backend.getRemotelyBufferedRange();
-
-      this.audioTracks = this.backend.getAudioTracks();
-      this.subtitleTracks = this.backend.getSubtitleTracks();
-    });
-    this.backend.addPassiveEventListener('durationchange', () => {
-      console.log('event: durationchange');
-      this.duration = this.backend.duration;
-      this.localBufferedRanges = this.backend.getBufferedRanges();
-      this.remoteBufferedRange = this.backend.getRemotelyBufferedRange();
-    });
-    this.backend.addPassiveEventListener('play', () => {
-      console.log('event: play');
-      this.isPlaying = true;
-    });
-    this.backend.addPassiveEventListener('pause', () => {
-      console.log('event: pause');
-      this.isPlaying = false;
-    });
-    this.backend.addPassiveEventListener('timeupdate', () => {
-      console.log('event: timeupdate');
-      this.currentTime = this.backend.currentTime;
-
-      this.localBufferedRanges = this.backend.getBufferedRanges();
-      this.remoteBufferedRange = this.backend.getRemotelyBufferedRange();
-    });
-    this.backend.addPassiveEventListener('volumechange', () => {
-      console.log('event: volumechange');
-      this.volume = this.backend.volume;
-      this.muted = this.backend.muted;
-    });
-    this.backend.addPassiveEventListener('progress', () => {
-      console.log('event: progress');
-      this.localBufferedRanges = this.backend.getBufferedRanges();
-      this.remoteBufferedRange = this.backend.getRemotelyBufferedRange();
-    });
-  }
-
   get $currentTime(): number {
     return this.currentTime;
   }
@@ -137,5 +94,42 @@ export default class VideoPlayer {
 
   destroy(): void {
     this.backend.destroy();
+  }
+
+  private setupEventListeners(): void {
+    this.backend.addPassiveEventListener('loadedmetadata', () => {
+      this.duration = this.backend.duration;
+      this.localBufferedRanges = this.backend.getBufferedRanges();
+      this.remoteBufferedRange = this.backend.getRemotelyBufferedRange();
+
+      this.audioTracks = this.backend.getAudioTracks();
+      this.subtitleTracks = this.backend.getSubtitleTracks();
+    });
+    this.backend.addPassiveEventListener('durationchange', () => {
+      this.duration = this.backend.duration;
+
+      this.localBufferedRanges = this.backend.getBufferedRanges();
+      this.remoteBufferedRange = this.backend.getRemotelyBufferedRange();
+    });
+    this.backend.addPassiveEventListener('play', () => {
+      this.isPlaying = true;
+    });
+    this.backend.addPassiveEventListener('pause', () => {
+      this.isPlaying = false;
+    });
+    this.backend.addPassiveEventListener('timeupdate', () => {
+      this.currentTime = this.backend.currentTime;
+
+      this.localBufferedRanges = this.backend.getBufferedRanges();
+      this.remoteBufferedRange = this.backend.getRemotelyBufferedRange();
+    });
+    this.backend.addPassiveEventListener('volumechange', () => {
+      this.volume = this.backend.volume;
+      this.muted = this.backend.muted;
+    });
+    this.backend.addPassiveEventListener('progress', () => {
+      this.localBufferedRanges = this.backend.getBufferedRanges();
+      this.remoteBufferedRange = this.backend.getRemotelyBufferedRange();
+    });
   }
 }
