e6e59130c581d946dad087cf1126dddcbd6a3908
Christian Koop <contact@sprax2013.de>
2025-07-19 01:33:38 +0200

    chore: update Svelte frontend dependencies

--- frontend/svelte.config.ts
+++ frontend/svelte.config.ts
@@ -1,8 +1,8 @@
 import adapter from '@sveltejs/adapter-node';
+import type { Config } from '@sveltejs/kit';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+const config: Config = {
   preprocess: vitePreprocess(),
 
   kit: {
