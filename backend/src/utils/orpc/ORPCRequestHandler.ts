16d77cd7cf98356a6fcdbe14de2d952aeb418c4e
Christian Koop <contact@sprax2013.de>
2026-01-01 14:56:27 +0100

    feat: configure oRPC's CORS plugin to only allow baseUrl as origin
    This does not have an effect when running the dev server.
    Vite (I assume) overwrites the header(s) that oRPC might set.
    So this option is essentially only relevant in production mode.

--- backend/src/utils/orpc/ORPCRequestHandler.ts
+++ backend/src/utils/orpc/ORPCRequestHandler.ts
@@ -1,17 +1,21 @@
 import * as oRpcNodeJs from '@orpc/server/node';
 import * as oRpcPlugins from '@orpc/server/plugins';
 import { singleton } from 'tsyringe';
+import AppConfiguration from '../../config/AppConfiguration.js';
 import type { ORPCInitialContext } from './oRpcRouteBuilder.js';
 import { oRpcRouter } from './oRpcRouter.js';
 
 @singleton()
 export default class ORPCRequestHandler extends oRpcNodeJs.RPCHandler<ORPCInitialContext> {
-  constructor() {
+  constructor(
+    appConfig: AppConfiguration,
+  ) {
     super(
       oRpcRouter,
       {
-        // FIXME: Configure CORS properly
-        plugins: [new oRpcPlugins.CORSPlugin()],
+        plugins: [new oRpcPlugins.CORSPlugin({
+          origin: () => appConfig.config.baseUrl,
+        })],
       },
     );
   }
