67c68d62aa1128fc1edecb71c8bb76d5ed6e2f2b
Christian Koop <contact@sprax2013.de>
2026-01-31 16:54:42 +0100

    fix(media): properly implement the season select in media detail view

--- frontend/src/lib/components/media/media/EpisodeList.svelte
+++ frontend/src/lib/components/media/media/EpisodeList.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { goto } from '$app/navigation';
+  import { page } from '$app/state';
   import EpisodeCard from '$lib/components/media/media/EpisodeCard.svelte';
 
   let { nextEpisodeIdToWatch, seasons }: {
@@ -19,20 +21,55 @@
     }[],
   } = $props();
 
-  const selectedSeasonNumber = $state(seasons[0].seasonNumber);
-  const episodesForSelectedSeason = $derived(
-    seasons.find(s => s.seasonNumber === selectedSeasonNumber)?.episodes ?? [],
-  );
+  let selectedSeasonIndex = $state(determineInitialSeasonIndex());
+
+  const episodesForSelectedSeason = $derived(seasons[selectedSeasonIndex]?.episodes ?? []);
   const episodeToWatchNext = $derived(episodesForSelectedSeason.find(e => e.id === nextEpisodeIdToWatch) ?? null);
+
+  function determineInitialSeasonIndex(): number {
+    const queryParamValue = parseInt(page.url.searchParams.get('s') || '1', 10);
+
+    const querySeasonIndex = seasons.findIndex(s => s.seasonNumber === queryParamValue);
+    if (querySeasonIndex !== -1) {
+      return querySeasonIndex;
+    }
+
+    const firstSeason = seasons.findIndex(s => s.seasonNumber === 1);
+    if (firstSeason !== -1) {
+      return firstSeason;
+    }
+
+    return 0;
+  }
+
+  function setSelectedSeasonIndex(index: number): void {
+    selectedSeasonIndex = index;
+    const selectedSeasonNumber = seasons[index]?.seasonNumber;
+
+    const url = new URL(window.location.href);
+
+    if (typeof selectedSeasonNumber !== 'number') {
+      if (url.searchParams.has('s')) {
+        url.searchParams.delete('s');
+        goto(url.href, { replaceState: true, keepFocus: true, noScroll: true });
+      }
+      return;
+    }
+
+    url.searchParams.set('s', selectedSeasonNumber.toString());
+    goto(url.href, { replaceState: true, keepFocus: true, noScroll: true });
+  }
 </script>
 
 <div class="d-flex justify-content-between align-items-center mb-4">
   <h2 class="h3 m-0">Episodes</h2>
   <select class="form-select bg-dark text-white border-secondary"
           style="width: auto"
-          aria-label="Select Season">
-    {#each seasons as season}
-      <option selected={season.seasonNumber === selectedSeasonNumber}>Season {season.seasonNumber}</option>
+          aria-label="Select Season"
+          onchange={(event) => setSelectedSeasonIndex(Math.max(event.currentTarget.selectedIndex, 0))}
+  >
+    {#each seasons as season, index}
+      <option selected={index === selectedSeasonIndex}>Season {season.seasonNumber}</option>
     {/each}
   </select>
 </div>
