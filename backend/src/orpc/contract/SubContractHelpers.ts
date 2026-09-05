d8e4bba2a9329f43b6dad98d7feb5975e683b602
Christian Koop <contact@sprax2013.de>
2026-09-05 17:51:55 +0200

    fix: set correct status codes for our pre-defined/global oRPC errors

--- backend/src/orpc/contract/SubContractHelpers.ts
+++ backend/src/orpc/contract/SubContractHelpers.ts
@@ -5,11 +5,11 @@ export const ERROR_CODE_REQUESTED_ENTITY_NOT_FOUND = 'REQUESTED_ENTITY_NOT_FOUND
 // TODO: Maybe we can type-hint errors on the individual contracts, instead of having *all* here
 export const baseOc = oc
   .errors({
-    UNAUTHORIZED: {},
-    NO_PERMISSIONS: {},
-    INVALID_INPUT: {},
-    NOT_AVAILABLE_FOR_LOGGED_IN_USER: {},
-    FEATURE_DISABLED: {},
-    [ERROR_CODE_REQUESTED_ENTITY_NOT_FOUND]: {},
-    UNSUPPORTED_FILE: {},
+    UNAUTHORIZED: { status: 401 },
+    NO_PERMISSIONS: { status: 403 },
+    INVALID_INPUT: { status: 400 },
+    NOT_AVAILABLE_FOR_LOGGED_IN_USER: { status: 409 },
+    FEATURE_DISABLED: { status: 403 },
+    [ERROR_CODE_REQUESTED_ENTITY_NOT_FOUND]: { status: 404 },
+    UNSUPPORTED_FILE: { status: 422 },
   });
